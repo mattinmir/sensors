7f3e85ee095816da7a9b23e3c846842021eeba9b
mattin <mattinmir@gmail.com>
2016-05-30 17:17:03 +0100

    added quotes to params

--- fhem_to_db/readlog.py
+++ fhem_to_db/readlog.py
@@ -1,12 +1,10 @@
 import subprocess
 
 perlfile = 'updatedb.pl'
-logfile = 'testlog.log'
+logfile = 'EnO_sensor_019B9ACD-2016.log'
 log = open(logfile)
 
 lines = [l for l in log if 'temperature' in l]
-for l in lines:
-    print(l)
 
 # Based on string being of the format '2016-05-27_15:01:21 EnO_sensor_019B9ACD temperature: 23.8'
 for l in lines:
@@ -16,9 +14,5 @@ for l in lines:
     timestamp = fields[0]
     value = float(fields[3].rstrip('\n'))
 
-    execute_string = 'perl ' + perlfile + ' ' + id + ' ' + timestamp + ' ' + value
+    execute_string = 'perl ' + perlfile + ' "' + id + '" "' + timestamp + '" ' + str(value)
     subprocess.call(execute_string)
-
-
-
-
