582f32663987af5237795df831a6f8f8dfc31d70
mattin <mattinmir@gmail.com>
2016-05-30 13:27:41 +0100

    updated script to read params and perl to right DB

--- fhem_to_db/readlog.py
+++ fhem_to_db/readlog.py
@@ -1,7 +1,24 @@
-logfile = 'testlog.log'
+import subprocess
 
+perlfile = 'updatedb.pl'
+logfile = 'testlog.log'
 log = open(logfile)
 
-lines = [l for l in lines if 'temperature' in l]
-
+lines = [l for l in log if 'temperature' in l]
 for l in lines:
+    print(l)
+
+# Based on string being of the format '2016-05-27_15:01:21 EnO_sensor_019B9ACD temperature: 23.8'
+for l in lines:
+    fields = l.split()
+
+    id = fields[1].split('_')[2]
+    timestamp = fields[0]
+    value = float(fields[3].rstrip('\n'))
+
+    execute_string = 'perl ' + perlfile + ' ' + id + ' ' + timestamp + ' ' + value
+    subprocess.call(execute_string)
+
+
+
+
