933cc672310364c1b84860a535f9e1216a91c89c
mattin <mattinmir@gmail.com>
2016-05-31 17:21:23 +0100

    works with polling by sometime truncates start of date

--- fhem_to_db/readlog.py
+++ fhem_to_db/readlog.py
@@ -1,18 +1,27 @@
 import subprocess
+import time
 
 perlfile = 'updatedb.pl'
-logfile = 'EnO_sensor_019B9ACD-2016.log'
+logfile = 'testlog.log'
 log = open(logfile)
 
-lines = [l for l in log if 'temperature' in l]
+#lines = [l for l in log if 'temperature' in l]
 
-# Based on string being of the format '2016-05-27_15:01:21 EnO_sensor_019B9ACD temperature: 23.8'
-for l in lines:
-    fields = l.split()
+# Go to end of file
+log.seek(0,2)
 
-    id = fields[1].split('_')[2]
-    timestamp = fields[0]
-    value = float(fields[3].rstrip('\n'))
+while True:
+    l = log.readline()
+    if not l:
+        time.sleep(0.1)
+    elif 'temperature' in l:
+        # Based on string being of the format '2016-05-27_15:01:21 EnO_sensor_019B9ACD temperature: 23.8'
+        fields = l.split()
 
-    execute_string = 'perl ' + perlfile + ' "' + id + '" "' + timestamp + '" ' + str(value)
-    subprocess.call(execute_string)
+        sensor_id = fields[1].split('_')[2]
+        timestamp = fields[0]
+        value = float(fields[3].rstrip('\n'))
+
+        execute_string = 'perl ' + perlfile + ' "' + sensor_id + '" "' + timestamp + '" ' + str(value)
+        # subprocess.call(execute_string)
+        print(execute_string)
