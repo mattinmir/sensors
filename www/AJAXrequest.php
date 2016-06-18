ae34ca656dc5ae73b308e04e44869b826e9e5e1a
Alex <Alex>
2016-06-18 16:27:38 +0100

    Removed unneccesary files

--- www/AJAXrequest.php
+++
@@ -1,3 +0,0 @@
-<?php
-$basedir = realpath(__DIR__);
-require_once ("/homepages/38/d629353585/htdocs/scripts/sql.php");
--- www/testvar.php
+++
@@ -1,6 +0,0 @@
-<?php
-var_dump($_SERVER);
-
-foreach (getallheaders() as $name => $value) {
-    echo "$name: $value\n";
-}
