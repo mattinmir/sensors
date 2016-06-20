7285a2a18dacec9bc50ecd03e1124cfa786e8708
mattinmir <mattinmir@users.noreply.github.com>
2016-06-21 00:34:40 +0100

    tidyup

--- network_initialisation/node_failed.py
+++ network_initialisation/node_failed.py
@@ -1,13 +1,4 @@
-#-------------------------------------------------------------------------------
-# Name:        node_failed
-# Purpose:
-#
-# Author:      mm5213
-#
-# Created:     17/06/2016
-# Copyright:   (c) mm5213 2016
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
+# Tells DB nodeID is active
 
 import requests
 import sys
@@ -15,6 +6,3 @@ import sys
 nodeID = sys.argv[1]
 r = requests.post('http://api.smartlandlords.co.uk/api.php/update/' + nodeID + '/status/failed/', data = {"auth":"YWRtaW46Z2lyYWZmZXM="})
 
-# Keep trying until it wokrs
-#while r.status_code != requests.codes.ok:
-   #r = requests.post('http://api.smartlandlords.co.uk/api.php/update/' + nodeID + '/status/failed/', data = {"auth":"YWRtaW46Z2lyYWZmZXM="})
