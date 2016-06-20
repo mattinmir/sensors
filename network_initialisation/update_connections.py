14b6fe0b585dac310c7d927594a30552faa336dc
mattinmir <mattinmir@users.noreply.github.com>
2016-06-21 00:39:31 +0100

    tidyup

--- network_initialisation/update_connections.py
+++ network_initialisation/update_connections.py
@@ -1,13 +1,4 @@
-#-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
-#
-# Author:      mm5213
-#
-# Created:     17/06/2016
-# Copyright:   (c) mm5213 2016
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
+# Tells DB how many connecitons sensorID has
 
 import requests
 import sys
@@ -15,7 +6,3 @@ import sys
 sensorID = sys.argv[1]
 connections = sys.argv[2]
 r = requests.post('http://api.smartlandlords.co.uk/api.php/update/' + sensorID + '/trans_connections/' + connections + '/', data = {"auth":"YWRtaW46Z2lyYWZmZXM="})
-
-# Keep trying until it wokrs
-#while (r.status_code != requests.codes.ok):
-   #r = requests.post('http://api.smartlandlords.co.uk/api.php/update/' + sensorID + '/trans_connections/' + connections + '/', data = {"auth":"YWRtaW46Z2lyYWZmZXM="})
