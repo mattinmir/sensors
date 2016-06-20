1505cbff2607b000985b5a65f2e7da49f497e239
mattinmir <mattinmir@users.noreply.github.com>
2016-06-21 00:33:24 +0100

    tidyup

--- network_initialisation/helpers.h
+++ network_initialisation/helpers.h
@@ -23,23 +23,14 @@ std::vector<std::string> &split(const std::string &s, char delim, std::vector<st
 
 std::vector<std::string> split(const std::string &s, char delim);
 
-double median_rssi(std::vector<double> rssis);
-
-
-//void new_connection(std::vector<Sensor> &sensors, std::string transID, std::string sensorID, double rssi);
-
 // Gets names of all files in `directory` with `extension`
 std::vector<std::string> get_file_list(std::string directory, std::string extension);
 
-
-void generate_whitelist(std::map<std::string, std::vector<std::string>> &whitelist, std::vector<std::string> failures, std::map<std::string, std::vector<std::string>> connections);
 
 void update_whitelist(std::map<std::string, std::vector<std::string>> &whitelist, std::map<std::string, Sensor> &sensors, std::set<std::string> &failures, bool &updated);
 
 // Checking for updated whitelist
 void check_for_update(std::string blacklistfilename, std::map<std::string, std::vector<std::string>> &whitelist, std::set<std::string> &db_transceievers, bool &updated);
-
-void update_rssis(std::map<std::string, Sensor> &sensors, std::string logfile_name, std::set<std::string> &db_sensors, std::set<std::string> &db_transceivers);
 
 void add_new_nodes(std::string sensorsfilename, std::set<std::string> &db_sensors, std::map<std::string, Sensor> &sensors, std::string transfilename, std::set<std::string> &db_transceivers, std::map<std::string, std::vector<std::string>> &whitelist);
 
